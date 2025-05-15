b1a49d35505aa6c69dbca7a7dc86aa6f4b2f6484
facundoscholze <facundoscholze@gmail.com>
2025-05-15 11:48:46 -0300

    arreglado total

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>¡Hola desde mi SaaS App! Soy Facu okabrito</h1>
+      <h1>¡Hola desde mi SaaS App! Soy Facu Scholze okabrito</h1>
       <p>Esto está corriendo en Railway 🚀</p>
     </main>
   );
